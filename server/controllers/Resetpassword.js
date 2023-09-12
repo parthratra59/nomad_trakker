78d8dcfbb6a7cd10752d8f2a937a112ec424d74e
Parth Ratra <ratraparth59@gmail.com>
2023-09-13 05:25:16 +0530

    bcryptjs module update

--- server/controllers/Resetpassword.js
+++ server/controllers/Resetpassword.js
@@ -1,5 +1,5 @@
 const User = require("../models/User");
-const bcrypt = require("bcrypt");
+const bcrypt = require("bcryptjs");
 const jwt = require("jsonwebtoken");
 const { mailSender } = require("../utils/mailsender");
 const crypto = require("crypto");
