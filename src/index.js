998000e0df845244ef5e3e667734d952c015ed76
Parth Ratra <ratraparth59@gmail.com>
2023-09-16 05:00:48 +0530

    minor bug fixed

--- src/index.js
+++ src/index.js
@@ -8,7 +8,7 @@ import { Toaster } from "react-hot-toast";
 // import Path from './Path';
 import { Provider } from "react-redux";
 import store from "./redux/store";
-// dfdfd
+// dfdfdldfdf
 ReactDOM.render(
   <>
     <BrowserRouter>
